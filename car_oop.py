3d1624472830c8ef8f4a4c3407dcd270c2de5cdb
HawiCaesar <brian.odhiambo932@gmail.com>
2017-06-13 14:01:09 +0300

    fixed the is_saloon logic

--- car_oop.py
+++ car_oop.py
@@ -1,14 +1,13 @@
 
 class Car(object):
 
-	def __init__(self, car_type="saloon", model="GM", name="General"):
-
-		car_list_exception = ['Porsche', 'Koenigsegg']
+	def __init__(self, name="General" ,model="GM", car_type=""):
 
 		self.car_type = car_type
 		self.model = model
 		self.name = name
 		self.speed = 0
+		
 		
 		# Except Porsche and Koenigsegg
 		if self.name in ['Porsche', 'Koenigsegg']:
@@ -16,14 +15,17 @@ class Car(object):
 		else:
 			self.number_of_doors = 4
 
-		if self.is_saloon() == False:
+
+		#check if car type is trailer
+		if self.car_type == "trailer":
 			self.number_of_wheels = 8
 		else:
 			self.number_of_wheels = 4
 
-	# Check if car is saloon
+
+	#Check if car is saloon based on wheels
 	def is_saloon(self):
-		if self.car_type == "trailer":
+		if self.number_of_wheels == 8:
 			return False
 		else:
 			return True
@@ -38,10 +40,10 @@ class Car(object):
 
 
 
-a = Car("saloon","TG","Porsche")
 b = Car("MAN","Truck","trailer")
 k= Car('Koenigsegg', 'Agera R')
 
-print(a.number_of_doors)
 print(k.number_of_wheels)
+print(b.number_of_wheels)
+print(b.is_saloon())
 
